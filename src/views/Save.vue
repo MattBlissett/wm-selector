82b66db396181e447225b171f47d0c0dd90e2cf4
Matthew Blissett <matt@blissett.me.uk>
2024-10-26 10:56:46 +0200

    Allow save to work when deployed under a path.

--- src/views/Save.vue
+++ src/views/Save.vue
@@ -14,7 +14,7 @@ import store from '@/store';
 export default {
   name: 'SaveView',
   methods: {
-    url: () => window.location.origin + '/#/?' + Querystring.stringify(Object.keys(store.getters.usedUnits)
+    url: () => window.location.origin + window.location.pathname + '#/?' + Querystring.stringify(Object.keys(store.getters.usedUnits)
       .reduce((url, unitID) => {
         var unit = store.getters.usedUnits[unitID];
 
